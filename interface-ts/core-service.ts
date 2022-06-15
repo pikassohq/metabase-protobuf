d31f32e9f00abeab03f63cce8bcd08dd4f89c780
tuongnt <mison201@gmail.com>
2022-06-15 17:40:53 +0700

    chore: update core service proto

---
+++ interface-ts/core-service.ts
@@ -0,0 +1,47 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import * as Long from 'long';
+import * as _m0 from 'protobufjs/minimal';
+import { Observable } from 'rxjs';
+import { Transaction, Request } from './transaction';
+import { Project, Request as Request1 } from './project';
+
+export const protobufPackage = 'CoreService';
+
+export const CORE_SERVICE_PACKAGE_NAME = 'CoreService';
+
+export interface CoreServiceClient {
+  updateTransactionById(request: Request, ...rest: any): Observable<Transaction>;
+
+  updateProjectById(request: Request1, ...rest: any): Observable<Project>;
+}
+
+export interface CoreServiceController {
+  updateTransactionById(request: Request, ...rest: any): Promise<Transaction> | Observable<Transaction> | Transaction;
+
+  updateProjectById(request: Request1, ...rest: any): Promise<Project> | Observable<Project> | Project;
+}
+
+export function CoreServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ['updateTransactionById', 'updateProjectById'];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod('CoreService', method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod('CoreService', method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const CORE_SERVICE_NAME = 'CoreService';
+
+// If you get a compile-error about 'Constructor<Long> and ... have no overlap',
+// add '--ts_proto_opt=esModuleInterop=true' as a flag when calling 'protoc'.
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
